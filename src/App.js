2ead56ac1d95fc6605a24ecacefcf19cff53cae1
Atul Anand <atul1anand@gmail.com>
2021-07-07 10:18:13 +0530

    Context implementation in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React, { useState } from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import Home from './containers/Home';
 import './App.css';
 import Route1 from './containers/Route1';
 import Route2 from './containers/Route2';
+import { TestContext } from './utils/context';
 
 function App() {
+  const [name, setName] = useState('Atul Anand');
   return (
-    <main>
-      <div>
-        <div className="App-header">App JS</div>
-      </div>
-      <Link to="/">Home</Link>
-      <Link to="/route1">Route1</Link>
-      <Link to="/route2">Route2</Link>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/route1" component={Route1} />
-        <Route path="/route2" component={Route2} />
-      </Switch>
-    </main>
+    <TestContext.Provider value={{ name }}>
+      <main>
+        <div>
+          <div className="App-header" onClick={() => setName(name === 'Atul Anand' ? 'Kumud Kumari' : 'Atul Anand')}>
+            <div>{name} app</div>
+            <div>Context implementation: See Route 2 ... Comp 2</div>
+          </div>
+        </div>
+        <Link to="/">Home</Link>
+        <Link to="/route1">Route1</Link>
+        <Link to="/route2">Route2</Link>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/route1" component={Route1} />
+          <Route path="/route2" component={Route2} />
+        </Switch>
+      </main>
+    </TestContext.Provider>
   );
 }
 
--- src/containers/Route2/components/Comp2/index.jsx
+++ src/containers/Route2/components/Comp2/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { TestContext } from '../../../../utils/context';
 
-const Route2Comp2 = () => <div>Route 2 ... Comp 2</div>;
+class Route2Comp2 extends React.Component {
+  render() {
+    let test = this.context;
+    return <div>Route 2 ... Comp 2 ({test.name})</div>;
+  }
+}
 
 export default Route2Comp2;
+
+Route2Comp2.contextType = TestContext;
